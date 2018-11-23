c6266c7eef43512b4e572abf70a52f9b9cf42d14
root <467tn.mi@gmail.com>
2018-11-23 18:39:29 +0900

    なおした

--- Inmon/CreateDir/DM3_Data/Sale.sql
+++ Inmon/CreateDir/DM3_Data/Sale.sql
@@ -1,10 +1,10 @@
 INSERT INTO Sale(Category_key,Stories_key,Sale_number,Sale_number_count)
-SELECT Category_key,Stories_key,SUM(test_inmon2.Sale.Sale_number),COUNT(test_inmon2.Sale.Sale_number)
- FROM Category,Stories,test_inmon2.Sale
+SELECT Category_key,Stories_key,SUM(Inmon_DWH.Sale.Sale_number),COUNT(Inmon_DWH.Sale.Sale_number)
+ FROM Category,Stories,Inmon_DWH.Sale
 WHERE
- test_inmon2.Sale.Category1_code = Category.Category1_code
+ Inmon_DWH.Sale.Category1_code = Category.Category1_code
  AND
- test_inmon2.Sale.Category2_code = Category.Category2_code
+ Inmon_DWH.Sale.Category2_code = Category.Category2_code
  AND
- test_inmon2.Sale.Stories_number = Stories.Stories_number
+ Inmon_DWH.Sale.Stories_number = Stories.Stories_number
 GROUP BY Category_key,Stories_key;
